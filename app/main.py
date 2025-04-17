cc9c5f722d1a8954edaa4b3593fd29dde6aa39cf
RomanSapunGit <sapunroma23@gmail.com>
2025-04-17 16:15:54 +0200

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def copy_file(command: str) -> None:
             and len(args) == 3
             and args[1] != args[2]):
         try:
-            with (open(args[1], "tr") as file_to_read,
-                  open(args[2], "a") as file_to_write):
+            with (open(args[1], "r") as file_to_read,
+                  open(args[2], "w") as file_to_write):
                 for line in file_to_read:
                     file_to_write.write(line)
         except FileNotFoundError:
