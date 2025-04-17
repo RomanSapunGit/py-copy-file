2dd56e31402517e7bec445859f77d70ed25f9201
RomanSapunGit <sapunroma23@gmail.com>
2025-04-17 16:12:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    args = command.split(" ")
+    if (args
+            and args[0] == "cp"
+            and len(args) == 3
+            and args[1] != args[2]):
+        try:
+            with (open(args[1], "tr") as file_to_read,
+                  open(args[2], "a") as file_to_write):
+                for line in file_to_read:
+                    file_to_write.write(line)
+        except FileNotFoundError:
+            pass
